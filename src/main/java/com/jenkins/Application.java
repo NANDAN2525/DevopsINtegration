53ea55b4fd572b4a0ddad7f98c26ad391009b9d9
Nandan55 <Nnakirekanti@hottopic.com>
2023-09-14 10:58:29 +0530

    added changes

--- src/main/java/com/jenkins/Application.java
+++ src/main/java/com/jenkins/Application.java
@@ -3,11 +3,15 @@ package com.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class Application {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Application Started application :");
 	}
 
 }
